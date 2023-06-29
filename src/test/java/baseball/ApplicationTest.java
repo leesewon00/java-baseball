034032919e25e5db6974ea60f07622809f9df7ee
leesewon00 <dltpdnjs2000@gmail.com>
2023-06-29 19:40:47 +0900

    이세원 과제 제출합니다(1)

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -22,9 +22,40 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 모든_경우의_수() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456",
+                            "416", "412", "312",
+                            "136", "132",
+                            "145", "124", "123",
+                            "2"
+                    );
+                    assertThat(output()).contains(
+                            "낫싱",
+                            "1볼", "2볼", "3볼",
+                            "1볼 1스트라이크", "2볼 1스트라이크",
+                            "1스트라이크", "2스트라이크", "3스트라이크",
+                            "게임 종료");
+                },
+                1, 2, 3
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("023"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
